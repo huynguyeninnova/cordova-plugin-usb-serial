b479dd1d72e78810b8ccad4527a68e20273fbba0
huynguyeninnova <huy.nguyenthanh@vn.innova.com>
2024-06-20 14:49:39 +0700

    feat: change to promise base

--- www/serial.d.ts
+++ www/serial.d.ts
@@ -19,66 +19,43 @@ export declare class Serial extends IonicNativePlugin {
     /**
      * Request permission to connect to a serial device
      * @param {SerialPermissionOptions} [options] Options used to request serial permissions for an unknown device
-     * @param {SuccessCallback} successCallback Success callback
-     * @param {ErrorCallback}  errorCallback Success callback
-     * @return {void} Returns a promise that resolves when permissions are granted
+     * @return {Promise<any>} Returns a promise that resolves when permissions are granted
      */
     // @ts-ignore
-    requestPermission(options?: SerialPermissionOptions, successCallback: Function, errorCallback: Function): void;
+    requestPermission(options?: SerialPermissionOptions): Promise<any>;
     /**
      * Open connection to a serial device
      * @param {SerialOpenOptions} options Options used to open serial connection
-     * @param {SuccessCallback} successCallback Success callback
-     * @param {ErrorCallback}  errorCallback Success callback
-     * @return {void} Returns a promise that resolves when the serial connection is opened
+     * @return {Promise<any>} Returns a promise that resolves when the serial connection is opened
      */
-    open(options: SerialOpenOptions, successCallback: Function, errorCallback: Function): void;
+    open(options: SerialOpenOptions): Promise<any>;
     /**
      * Write to a serial connection
      *
      * @param data {any} data to write to the serial connection
-     * @param {SuccessCallback} successCallback Success callback
-     * @param {ErrorCallback}  errorCallback Success callback
-     * @return {void} Returns a promise that resolves when the write is complete
+     * @return {Promise<any>} Returns a promise that resolves when the write is complete
      */
-    write(data: any, successCallback: Function, errorCallback: Function): void;
+    write(data: any): Promise<any>;
     /**
      * Write hex to a serial connection
      *
      * @param data {any} data to write to the serial connection
-     * @param {SuccessCallback} successCallback Success callback
-     * @param {ErrorCallback}  errorCallback Success callback
-     * @return {void} Returns a promise that resolves when the write is complete
+     * @return {Promise<any>} Returns a promise that resolves when the write is complete
      */
-    writeHex(data: any, successCallback: Function, errorCallback: Function): void;
+    writeHex(data: any): Promise<any>;
     /**
      * Read from a serial connection
-     * @param {SuccessCallback} successCallback Success callback
-     * @param {ErrorCallback}  errorCallback Success callback
-     * @return {void} Returns a promise that resolves with data read from the serial connection
+     * @return {Promise<any>} Returns a promise that resolves with data read from the serial connection
      */
-    read(successCallback: Function, errorCallback: Function): void;
+    read(): Promise<any>;
     /**
      * Watch the incoming data from the serial connection. Clear the watch by unsubscribing from the observable
-     * @param {SuccessCallback} successCallback Success callback
-     * @param {ErrorCallback}  errorCallback Success callback
      * @returns {Observable<any>} Observable returns an observable that you can subscribe to
      */
-    registerReadCallback(successCallback: Function, errorCallback: Function): Observable<any>;
+    registerReadCallback(): Observable<any>;
     /**
      * Close the serial connection
-     * @param {SuccessCallback} successCallback Success callback
-     * @param {ErrorCallback}  errorCallback Success callback
-     * @return {void} Returns a promise that resolves when the serial connection is closed
+     * @return {Promise<any>} Returns a promise that resolves when the serial connection is closed
      */
-    close(successCallback: Function, errorCallback: Function): void;
-    /**
-     * This callback is displayed as a global member.
-     * @callback SuccessCallback
-     */
-    /**
-     * This callback is displayed as a global member.
-     * @callback ErrorCallback
-     * @param {any} e Error
-     */
+    close(): Promise<any>;
 }
--- www/serial.js
+++ www/serial.js
@@ -1,44 +1,80 @@
 const serial = {
   requestPermission: function (opts) {
-    return new Promise((resolve, reject)=>{
+    return new Promise((resolve, reject) => {
       cordova.exec(
-        (r)=>resolve(r),
-        (e)=>reject(e),
+        (r) => resolve(r),
+        (e) => reject(e),
         "Serial",
         "requestPermission",
-        [{ opts: opts }],
+        [{ opts: opts }]
       );
-    })
+    });
   },
-  open: function (opts, successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, "Serial", "openSerial", [
-      { opts: opts },
-    ]);
+  open: function (opts) {
+    return new Promise((resolve, reject) => {
+      cordova.exec(
+        (r) => resolve(r),
+        (e) => reject(e),
+        "Serial",
+        "openSerial",
+        [{ opts: opts }]
+      );
+    });
   },
-  write: function (data, successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, "Serial", "writeSerial", [
-      { data: data },
-    ]);
+  write: function (data) {
+    return new Promise((resolve, reject) => {
+      cordova.exec(
+        (r) => resolve(r),
+        (e) => reject(e),
+        "Serial",
+        "writeSerial",
+        [{ data: data }]
+      );
+    });
   },
-  writeHex: function (hexString, successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, "Serial", "writeSerialHex", [
-      { data: hexString },
-    ]);
+  writeHex: function (hexString) {
+    return new Promise((resolve, reject) => {
+      cordova.exec(
+        (r) => resolve(r),
+        (e) => reject(e),
+        "Serial",
+        "writeSerialHex",
+        [{ data: hexString }]
+      );
+    });
   },
-  read: function (successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, "Serial", "readSerial", []);
+  read: function () {
+    return new Promise((resolve, reject) => {
+      cordova.exec(
+        (r) => resolve(r),
+        (e) => reject(e),
+        "Serial",
+        "readSerial",
+        []
+      );
+    });
   },
-  close: function (successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, "Serial", "closeSerial", []);
+  close: function () {
+    return new Promise((resolve, reject) => {
+      cordova.exec(
+        (r) => resolve(r),
+        (e) => reject(e),
+        "Serial",
+        "closeSerial",
+        []
+      );
+    });
   },
-  registerReadCallback: function (successCallback, errorCallback) {
-    cordova.exec(
-      successCallback,
-      errorCallback,
-      "Serial",
-      "registerReadCallback",
-      [],
-    );
+  registerReadCallback: function () {
+    return new Promise((resolve, reject) => {
+      cordova.exec(
+        (r) => resolve(r),
+        (e) => reject(e),
+        "Serial",
+        "registerReadCallback",
+        []
+      );
+    });
   },
 };
 module.exports = serial;
