015e9c9c25f8bd2325492534d786b3bfb9214f53
huynguyeninnova <huy.nguyenthanh@vn.innova.com>
2024-06-20 11:04:30 +0700

    fix: declare file

--- www/serial.d.ts
+++ www/serial.d.ts
@@ -15,46 +15,30 @@ export interface SerialOpenOptions {
     sleepOnPause?: boolean;
 }
 /**
- * @name Serial
- * @description
- * This plugin provides functions for working with Serial connections
- *
- * @usage
- * ```typescript
- * import { Serial } from '@ionic-native/serial/ngx';
- *
- * constructor(private serial: Serial) { }
- *
- * ...
- *
- * this.serial.requestPermission().then(() => {
- *   this.serial.open({
- *     baudRate: 9800,
- *     dataBits: 4,
- *     stopBits: 1,
- *     parity: 0,
- *     dtr: true,
- *     rts: true,
- *     sleepOnPause: false
- *   }).then(() => {
- *     console.log('Serial connection opened');
- *   });
- * }).catch((error: any) => console.log(error));
- *
- * ```
+ * This callback is displayed as a global member.
+ * @callback SuccessCallback
  */
+/**
+ * This callback is displayed as a global member.
+ * @callback ErrorCallback
+ * @param {any} e Error
+ */
+
 export declare class SerialOriginal extends IonicNativePlugin {
     /**
      * Request permission to connect to a serial device
-     *
-     * @param options {SerialPermissionOptions} Options used to request serial permissions for an unknown device
+     * @param {SerialPermissionOptions} [options] Options used to request serial permissions for an unknown device
+     * @param {SuccessCallback} successCallback Success callback
+     * @param {ErrorCallback}  errorCallback Success callback
      * @return {void} Returns a promise that resolves when permissions are granted
      */
+    // @ts-ignore
     requestPermission(options?: SerialPermissionOptions, successCallback: Function, errorCallback: Function): void;
     /**
      * Open connection to a serial device
-     *
-     * @param options {SerialOpenOptions} Options used to open serial connection
+     * @param {SerialOpenOptions} options Options used to open serial connection
+     * @param {SuccessCallback} successCallback Success callback
+     * @param {ErrorCallback}  errorCallback Success callback
      * @return {void} Returns a promise that resolves when the serial connection is opened
      */
     open(options: SerialOpenOptions, successCallback: Function, errorCallback: Function): void;
@@ -62,6 +46,8 @@ export declare class SerialOriginal extends IonicNativePlugin {
      * Write to a serial connection
      *
      * @param data {any} data to write to the serial connection
+     * @param {SuccessCallback} successCallback Success callback
+     * @param {ErrorCallback}  errorCallback Success callback
      * @return {void} Returns a promise that resolves when the write is complete
      */
     write(data: any, successCallback: Function, errorCallback: Function): void;
@@ -69,24 +55,29 @@ export declare class SerialOriginal extends IonicNativePlugin {
      * Write hex to a serial connection
      *
      * @param data {any} data to write to the serial connection
+     * @param {SuccessCallback} successCallback Success callback
+     * @param {ErrorCallback}  errorCallback Success callback
      * @return {void} Returns a promise that resolves when the write is complete
      */
     writeHex(data: any, successCallback: Function, errorCallback: Function): void;
     /**
      * Read from a serial connection
-     *
+     * @param {SuccessCallback} successCallback Success callback
+     * @param {ErrorCallback}  errorCallback Success callback
      * @return {void} Returns a promise that resolves with data read from the serial connection
      */
     read(successCallback: Function, errorCallback: Function): void;
     /**
      * Watch the incoming data from the serial connection. Clear the watch by unsubscribing from the observable
-     *
+     * @param {SuccessCallback} successCallback Success callback
+     * @param {ErrorCallback}  errorCallback Success callback
      * @returns {Observable<any>} Observable returns an observable that you can subscribe to
      */
     registerReadCallback(successCallback: Function, errorCallback: Function): Observable<any>;
     /**
      * Close the serial connection
-     *
+     * @param {SuccessCallback} successCallback Success callback
+     * @param {ErrorCallback}  errorCallback Success callback
      * @return {void} Returns a promise that resolves when the serial connection is closed
      */
     close(successCallback: Function, errorCallback: Function): void;
